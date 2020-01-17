469acadc0d30f6a9e8ed52cf4d3806c7b515fa70
vandlaw7 <vandlaw@naver.com>
2020-01-18 03:25:57 +0900

    so tired

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'short_desc', 'price', 'is_publish']
+    list_display_links = ['name']
+    list_filter = ['is_publish', 'updated_at']
+    search_fields = ['name']
+
+    def short_desc(self, item):
+        return item.desc[:20]
--- shop/models.py
+++ shop/models.py
@@ -5,10 +5,11 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
+    is_publish = models.BooleanField(default= False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
 
-class Comment(models.Model):
-    post = models.ForeignKey(Item, on_delete=models.CASCADE)
-    message = models.TextField
+
